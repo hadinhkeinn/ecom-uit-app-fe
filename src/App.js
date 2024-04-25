10e117dcb46a3f9d8115a9c0d61fe14f8ceefa79
dinkien <21522244@gm.uit.edu.vn>
2024-04-25 15:54:41 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/home/Home';
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
