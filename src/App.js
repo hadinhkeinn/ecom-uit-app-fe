70e2389f900b354ba3c441ca7f0e948646940cac
nguyenlongtruong <nguyenlongtruong2003@gmail.com>
2024-04-27 21:16:12 +0700

    header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/home/Home';
+import Header from './components/header/Header';
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
