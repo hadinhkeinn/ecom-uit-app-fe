d3a7800473f576ef57c3515df5d8a474be97508f
billythephes <21521291@gm.uit.edu.vn>
2024-04-27 21:26:57 +0700

    add carouselitem component

---
+++ src/components/carousel/data.js
@@ -0,0 +1,84 @@
+export const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 4,
+    slidesToSlide: 1, // optional, default to 1.
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+    slidesToSlide: 1, // optional, default to 1.
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+    slidesToSlide: 1, // optional, default to 1.
+  },
+};
+
+export const productData = [
+  {
+    id: 1,
+    imageurl:
+      "https://nshopvn.com/wp-content/uploads/2023/07/ga-xi-lanh-khi-nen-mal-16-sdb-16-mfk4-1-1-300x300.jpg",
+    name: "Gá xi lanh khí nén MAL-16 SDB-16",
+    price: "23.000",
+    description: "Some text about the product..",
+  },
+  {
+    id: 2,
+    imageurl:
+      "https://nshopvn.com/wp-content/uploads/2024/04/mach-sac-nhanh-type-c-pd3-1-pps-65w-dau-vao-jack-dc-5-5x2-1mm-sv7m-1-300x300.jpg",
+    name: "Mạch sạc nhanh Type C PD3.1 (PPS) 65W đầu vào jack DC 5.5x2.1mm",
+    price: "65.000",
+    description: "Some text about the product..",
+  },
+  {
+    id: 3,
+    imageurl:
+      "https://nshopvn.com/wp-content/uploads/2024/04/puly-gt2-48-rang-duong-kinh-30mm-truc-8mm-2d5b-1-300x300.jpg",
+    name: "Puly GT2 48 răng đường kính 30mm trục 8mm",
+    price: "43.000",
+    description: "Some text about the product..",
+  },
+  {
+    id: 4,
+    imageurl:
+      "https://nshopvn.com/wp-content/uploads/2022/08/quat-tan-nhiet-it-tieng-on-7025-7cm-5vdc-xnka-1-300x300.jpg",
+    name: "Quạt tản nhiệt ít tiếng ồn 7025 7cm 5VDC",
+    price: "25.000",
+    description: "Some text about the product..",
+  },
+  {
+    id: 5,
+    imageurl:
+      "https://nshopvn.com/wp-content/uploads/2024/04/mo-han-cell-pin-jaston-jsd-3180-vtue-1-300x300.jpg",
+    name: "Mỡ hàn cell pin JASTON JSD-3180",
+    price: "35.000",
+    description: "Some text about the product..",
+  },
+  {
+    id: 6,
+    imageurl:
+      "https://nshopvn.com/wp-content/uploads/2020/04/module-nguon-ac-dc-hi-link-9vdc-3w-qlbs-1-300x300.jpg",
+    name: "Module Nguồn AC-DC Hi-Link 9VDC - 3W",
+    price: "68.000",
+    description: "Some text about the product..",
+  },
+  {
+    id: 7,
+    imageurl:
+      "https://nshopvn.com/wp-content/uploads/2023/09/board-arduino-nano-3-0-atmega328p-type-c-27v1-1-300x300.jpg",
+    name: "Board Arduino Nano 3.0 ATmega328P Type-C",
+    price: "105.000",
+    description: "Some text about the product..",
+  },
+  {
+    id: 8,
+    imageurl:
+      "https://nshopvn.com/wp-content/uploads/2019/03/cam-bien-toc-do-gio-W10W-2020-300x300.jpg",
+    name: "Cảm biến tốc độ gió",
+    price: "1.557.000",
+    description: "Some text about the product..",
+  },
+];
