fa7d931e5f36bcad14d87ee7e56b847f7946cddd
KhaiNoob <danhhoangkhai07@gmail.com>
2024-04-28 21:06:00 +0700

    selectively display nav links

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,6 +6,7 @@ import { HiOutlineMenuAlt3 } from "react-icons/hi";
 import { FaTimes } from "react-icons/fa";
 import { useDispatch } from 'react-redux';
 import { RESET_AUTH, logout } from '../../redux/features/auth/authSlice';
+import ShowOnLogin, { ShowOnLogout } from '../hiddenLink/hiddenLink';
 
 export const logo = (
     <div className={styles.logo}>
@@ -78,18 +79,26 @@ const logoutUser = async () => {
 
                 <div className={styles["header-right"]}>
                     <span className={styles.links}>
+                    <ShowOnLogout>
                         <NavLink to={"login"} className={activeLink}>
                             Login
                         </NavLink>
+                        </ShowOnLogout>
+                        <ShowOnLogout>
                         <NavLink to={"register"} className={activeLink}>
                             Register
                         </NavLink>
+                        </ShowOnLogout>
+                        <ShowOnLogin>
                         <NavLink to={"order-history"} className={activeLink}>
                             My order
                         </NavLink>
-                        <NavLink to={"/"} onClick={logoutUser}>
+                        </ShowOnLogin>
+                        <ShowOnLogin>
+                        <Link to={"/"} onClick={logoutUser}>
                             Logout
-                        </NavLink>
+                        </Link>
+                        </ShowOnLogin>
                     </span>
                     {cart}
                 </div>
