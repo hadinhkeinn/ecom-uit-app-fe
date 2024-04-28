5fb28ff4023b75bec57cea71333c9d39ae15653c
billythephes <21521291@gm.uit.edu.vn>
2024-04-28 10:13:40 +0700

    add footer links component

---
+++ src/components/footer/FooterLinks.js
@@ -0,0 +1,110 @@
+import React from 'react'
+import "./FooterLinks.scss"
+import { FaFacebookF, FaTwitter, FaInstagram, FaYoutube } from "react-icons/fa"
+import logoImg from "../../assets/shopito_logo.png" 
+
+const FooterLinks = () => {
+  return <>
+    <section className='contact-section'>
+        <div className='container contact'>
+            <div className='contact-icon'>
+                <FaFacebookF className="i"/>
+                <FaTwitter className="i"/>
+                <FaInstagram className="i"/>
+                <FaYoutube className="i"/>
+            </div>
+            <h2>Let's Talk?</h2>
+            <a href='#home' className='btn btn-dark'>Liên hệ ngay!</a>
+        </div>
+    </section>
+
+    <section className='footer-section'>
+        <div className='container footer'>
+            <div className='footer-logo'>
+                <img src={logoImg} alt="logo" />
+            </div>
+            <div className='footer-menu'>
+                <p className='link-heading'>
+                    Tính năng
+                </p>
+                <ul className='nav-ul footer-links'>
+                    <li>
+                        <a href='#home'>Liên kết rút gọn</a>
+                    </li>
+                    <li>
+                        <a href='#home'>Liên kết thương hiệu</a>
+                    </li>
+                    <li>
+                        <a href='#home'>Phân tích</a>
+                    </li>
+                    <li>
+                        <a href='#home'>Bài viết</a>
+                    </li>
+                </ul>  
+            </div>
+
+            <div className='footer-menu'>
+                <p className='link-heading'>
+                    Tài nguyên
+                </p>
+                <ul className='nav-ul footer-links'>
+                    <li>
+                        <a href='#home'>Bài viết</a>
+                    </li>
+                    <li>
+                        <a href='#home'>Nhà phát triển</a>
+                    </li>
+                    <li>
+                        <a href='#home'>Hỗ trợ</a>
+                    </li>
+                    <li>
+                        <a href='#home'>Tài liệu</a>
+                    </li>
+                </ul>  
+            </div>
+
+            <div className='footer-menu'>
+                <p className='link-heading'>
+                    Công ty
+                </p>
+                <ul className='nav-ul footer-links'>
+                    <li>
+                        <a href='#home'>Giới thiệu</a>
+                    </li>
+                    <li>
+                        <a href='#home'>Đội của chúng tôi</a>
+                    </li>
+                    <li>
+                        <a href='#home'>Sự nghiệp</a>
+                    </li>
+                    <li>
+                        <a href='#home'>Liên hệ</a>
+                    </li>
+                </ul>  
+            </div>
+
+            <div className='footer-menu'>
+                <p className='link-heading'>
+                    Đối tác
+                </p>
+                <ul className='nav-ul footer-links'>
+                    <li>
+                        <a href='#home'>Mạng lưới</a>
+                    </li>
+                    <li>
+                        <a href='#home'>Hợp tác</a>
+                    </li>
+                    <li>
+                        <a href='#home'>Câu chuyện thành công</a>
+                    </li>
+                    <li>
+                        <a href='#home'>Tham gia cùng chúng tôi</a>
+                    </li>
+                </ul>  
+            </div>
+        </div>
+    </section>
+    </>
+}
+
+export default FooterLinks
